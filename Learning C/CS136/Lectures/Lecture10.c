a19f1858a76094ca7032234532ca18d6880f90c0
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-02-13 15:49:39 -0500

    Update Lecture10.c

--- Learning C/CS136/Lectures/Lecture10.c
+++ Learning C/CS136/Lectures/Lecture10.c
@@ -11,6 +11,7 @@ void swap(int  *a, int *b) {
     *b = tmp;
 }
 
+// prints all the elements in an array
 void print_arr(int a[], int len) {
     for (int i = 0; i < len; i++) {
         printf("%d", a[i]);
@@ -19,6 +20,7 @@ void print_arr(int a[], int len) {
     printf("\n");
 }
 
+// finds the index of the smallest number
 int find_smallest_index(int arr[], int start, int len) {
     int smallest_index = 0;
     for (int i = start; i < len; ++i) {
@@ -29,6 +31,12 @@ int find_smallest_index(int arr[], int start, int len) {
     return smallest_index;
 }
 
+// sorting algorithms:
+    // selection sort, insertion sort, quicksort
+
+    // selection sort
+    // find the smallest item in the list & swap it with the first pisition, then find next smallest & keep going
+
 void selection_sort(int arr[], int len) {
     int smallest_index = 0;
     for (int i = 0; i < len; ++i) {
@@ -38,6 +46,7 @@ void selection_sort(int arr[], int len) {
     }
 }
 
+// insertion sort -- compares to all the values before it & inserts itself in the proper position
 void insertion_sort(int a[], int len) {
     for (int i = 1; i < len; ++i) {
         for (int j = i; j > 0 && a[j - 1] > a[j]; --j) {
@@ -46,6 +55,7 @@ void insertion_sort(int a[], int len) {
     }
 }
 
+// quicksort 
 void quick_sort_range(int a[], int first, int last) {
     if (last <= first) return;
 
@@ -68,6 +78,7 @@ void quick_sort(int a[], int len) {
     quick_sort_range(a, 0, len - 1);
 }
 
+// binary search
 int binary_search(const int target, int arr[], int len) {
     int left = 0;
     int right = len - 1;
@@ -85,14 +96,20 @@ int binary_search(const int target, int arr[], int len) {
     }
 }
 
+// print a matrix 
+void print_matrix(const int a[], const int width, const int height) {
+    for (int i = 0; i < height; i ++) {
+        printf("[");
+        for (int j = 0; j < width; ++j) {
+            print(" %d", a[j * i * width]);
+        }
+        printf("]");
+    }
+}
+
 int main() {
     int a[5] = {1, 2, 3, 4, 5};
     int *p = a + 4; //sets to the 4th index, which has a value of 5
     int x = *(p = p[-2]); //5 - 3 = 2
 
-    // sorting algorithms:
-    // selection sort, insertion sort, quicksort
-
-    // selection sort
-    // find the smallest item in the list & swap it with the first pisition, then find next smallest & keep going
 }
