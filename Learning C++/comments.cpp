db7469f06b15b9283006d356dad945a7af0cc419
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-02-19 22:40:24 -0500

    learning c++

---
+++ Learning C++/comments.cpp
@@ -0,0 +1,14 @@
+/* 
+The function below takes
+in two integers and returns
+their product. 
+*/
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    // Prints: Codecademy rocks!
+    // std::coup stands for character output
+    std::coup << "Comments are good"
+}
