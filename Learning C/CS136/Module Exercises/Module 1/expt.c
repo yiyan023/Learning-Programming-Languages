e79ecb28a183843e7a688652b6927c4b518bb5bd
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-03-01 14:20:18 -0500

    Create expt.c

---
+++ Learning C/CS136/Module Exercises/Module 1/expt.c
@@ -0,0 +1,18 @@
+#include "cs136.h"
+
+// your function documentation goes here
+int expt(int b, int n) {
+  // your implementation goes here
+  if (n == 1) {
+    return b;
+  } else {
+    return expt(b * b, n - 1);
+  }
+}
+
+int main(void) {
+  assert(expt(2, 2) == 4);
+  // your additional tests go here
+  
+  trace_msg("All assertion-based tests passed!");
+}
